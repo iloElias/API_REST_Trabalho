2ff28db9bd0d4a963435327f30f5604db4f1657d
Blackburn060 <Blacktauz30@outlook.com>
2023-09-18 01:51:43 -0300

    Implementação de autenticação via token JWT

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Importe as dependências
 const express = require('express');
 const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
 
 // Inicialize o aplicativo Express
 const app = express();
@@ -12,71 +13,70 @@ const usuarios = [];
 const carrinhoDeCompras = [];
 const pedidos = [];
 
-class Usuario {
-    constructor(id, nome, email, senha) {
-        this.id = id;
-        this.nome = nome;
-        this.email = email;
-        this.senha = senha;
-    }
-}
+// Chave secreta para assinar os tokens JWT
+const secretKey = 'v4513hv46h3v41hvkj';
 
-// Função para verificar a autenticidade do usuário
-function autenticarUsuario(email, senha) {
-    const usuario = usuarios.find(u => u.email === email && u.senha === senha);
-    return usuario;
-}
+// Simulação de banco de dados de usuários
+const users = [];
 
-// Middleware de autenticação
-function autenticacaoMiddleware(req, res, next) {
-    const { email, senha } = req.body;
-    const usuarioAutenticado = autenticarUsuario(email, senha);
-    if (!usuarioAutenticado) {
-        return res.status(401).json({ message: 'Autenticação falhou' });
-    }
-    req.usuarioAutenticado = usuarioAutenticado;
+// Middleware de autenticação com JWT
+function authenticateToken(req, res, next) {
+  const token = req.header('Authorization');
+  if (!token) return res.status(401).send('Acesso negado. Token não fornecido.');
+
+  jwt.verify(token, secretKey, (err, user) => {
+    if (err) return res.status(403).send('Token inválido.');
+    req.user = user;
     next();
+  });
 }
 
 // Endpoint para cadastrar um novo usuário
-app.post('/usuarios/novo', (req, res) => {
-    const { nome, email, senha } = req.body;
-    // Verifique se o email já está em uso (você pode melhorar isso com um banco de dados)
-    const emailExistente = usuarios.find(u => u.email === email);
-    if (emailExistente) {
-        return res.status(400).json({ message: 'Email já está em uso' });
-    }
-    const novoUsuario = new Usuario(usuarios.length + 1, nome, email, senha);
-    usuarios.push(novoUsuario);
-    res.status(201).json(novoUsuario);
+app.post('/registro', (req, res) => {
+  const { username, password } = req.body;
+
+  // Verifica se o usuário já existe
+  const existingUser = users.find((user) => user.username === username);
+  if (existingUser) {
+    return res.status(400).send('Usuário já existe.');
+  }
+
+  // Cria um novo usuário
+  const newUser = { username, password };
+  users.push(newUser);
+
+  // Cria e assina um token JWT para o novo usuário
+  const token = jwt.sign({ username }, secretKey);
+  res.status(201).json({ token });
 });
 
-// Endpoint para exibir todos os usuários cadastrados (somente para fins de depuração)
-app.get('/usuarios', (req, res) => {
-    res.json(usuarios);
-});
+// Endpoint para realizar login
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
 
-// Middleware de autenticação para os próximos endpoints
-app.use(autenticacaoMiddleware);
+  // Verifica se o usuário existe
+  const user = users.find((user) => user.username === username);
+  if (!user) {
+    return res.status(401).send('Credenciais inválidas.');
+  }
 
-// Endpoint para exibir um usuário específico
-app.get('/usuarios/:id', (req, res) => {
-    const id = req.params.id;
-    const usuario = usuarios.find(u => u.id === Number(id));
-    if (!usuario) {
-        res.status(404).json({ message: 'Usuário não encontrado' });
-    } else {
-        res.json(usuario);
-    }
+  // Autentica o usuário
+  if (user.password === password) {
+    // Cria e assina um token JWT para o usuário autenticado
+    const token = jwt.sign({ username }, secretKey);
+    res.json({ token });
+  } else {
+    res.status(401).send('Credenciais inválidas.');
+  }
 });
 
 // Endpoint para recuperar todos os produtos
-app.get('/produtos', (req, res) => {
+app.get('/produtos', authenticateToken, (req, res) => {
     res.json(produtos);
 });
 
 // Endpoint para recuperar um produto específico por ID
-app.get('/produtos/:id', (req, res) => {
+app.get('/produtos/:id', authenticateToken, (req, res) => {
     const id = req.params.id;
     const produto = produtos.find(p => p.id === id);
     if (!produto) {
@@ -87,14 +87,14 @@ app.get('/produtos/:id', (req, res) => {
 });
 
 // Endpoint para criar um novo produto
-app.post('/produtos', (req, res) => {
+app.post('/produtos', authenticateToken, (req, res) => {
     const novoProduto = req.body;
     produtos.push(novoProduto);
     res.status(201).json(novoProduto);
 });
 
 // Endpoint para atualizar um produto existente por ID
-app.put('/produtos/:id', (req, res) => {
+app.put('/produtos/:id', authenticateToken, (req, res) => {
     const id = req.params.id;
     const atualizacaoProduto = req.body;
     const produtoIndex = produtos.findIndex(p => p.id === id);
@@ -107,7 +107,7 @@ app.put('/produtos/:id', (req, res) => {
 });
 
 // Endpoint para remover um produto por ID
-app.delete('/produtos/:id', (req, res) => {
+app.delete('/produtos/:id', authenticateToken, (req, res) => {
     const id = req.params.id;
     const produtoIndex = produtos.findIndex(p => p.id === id);
     if (produtoIndex === -1) {
@@ -119,7 +119,7 @@ app.delete('/produtos/:id', (req, res) => {
 });
 
 // Endpoint para recuperar o conteúdo atual do carrinho de compras de um usuário
-app.get('/carrinho', (req, res) => {
+app.get('/carrinho', authenticateToken, (req, res) => {
     // Verifique se o usuário autenticado está associado a um carrinho de compras
     const carrinhoUsuario = carrinhoDeCompras.find(c => c.usuarioId === req.usuarioAutenticado.id);
     if (!carrinhoUsuario) {
@@ -129,10 +129,8 @@ app.get('/carrinho', (req, res) => {
 });
 
 // Endpoint para adicionar um produto ao carrinho de compras
-app.post('/carrinho/adicionar', (req, res) => {
+app.post('/carrinho/adicionar', authenticateToken, (req, res) => {
     const { produtoId, quantidade } = req.body;
-    // Verifique se o produto existe (você deve implementar a lógica para isso)
-    // e se o usuário autenticado tem um carrinho de compras
     const produtoExistente = produtos.find(p => p.id === produtoId);
     if (!produtoExistente) {
         return res.status(404).json({ message: 'Produto não encontrado' });
@@ -156,10 +154,9 @@ app.post('/carrinho/adicionar', (req, res) => {
     res.status(201).json({ message: 'Produto adicionado ao carrinho com sucesso' });
 });
 
-// ... Definição dos modelos de carrinho de compras, pedidos, produtos, usuários e endpoints anteriores ...
 
 // Endpoint para recuperar a lista de todos os pedidos feitos pelo usuário
-app.get('/pedidos', (req, res) => {
+app.get('/pedidos', authenticateToken, (req, res) => {
     // Filtra os pedidos do usuário autenticado
     const pedidosUsuario = pedidos.filter(p => p.usuarioId === req.usuarioAutenticado.id);
     res.json(pedidosUsuario);
@@ -176,7 +173,7 @@ app.get('/pedidos/:id', (req, res) => {
 });
 
 // Endpoint para criar um novo pedido com base no conteúdo do carrinho de compras
-app.post('/pedidos/criar', (req, res) => {
+app.post('/pedidos/criar', authenticateToken, (req, res) => {
     // Obtenha o conteúdo do carrinho de compras do usuário autenticado
     const carrinhoUsuario = carrinhoDeCompras.find(c => c.usuarioId === req.usuarioAutenticado.id);
     if (!carrinhoUsuario || carrinhoUsuario.produtos.length === 0) {
@@ -187,7 +184,7 @@ app.post('/pedidos/criar', (req, res) => {
         id: pedidos.length + 1,
         usuarioId: req.usuarioAutenticado.id,
         produtos: carrinhoUsuario.produtos,
-        status: 'processando', // Você pode definir um status padrão aqui
+        status: 'processando',
     };
     pedidos.push(novoPedido);
     // Limpe o carrinho de compras do usuário
@@ -196,7 +193,7 @@ app.post('/pedidos/criar', (req, res) => {
 });
 
 // Endpoint para atualizar o status de um pedido
-app.put('/pedidos/atualizar/:id', (req, res) => {
+app.put('/pedidos/atualizar/:id', authenticateToken, (req, res) => {
     const id = req.params.id;
     const novoStatus = req.body.status;
     const pedido = pedidos.find(p => p.id === Number(id) && p.usuarioId === req.usuarioAutenticado.id);
@@ -208,7 +205,7 @@ app.put('/pedidos/atualizar/:id', (req, res) => {
 });
 
 // Endpoint para cancelar um pedido
-app.delete('/pedidos/cancelar/:id', (req, res) => {
+app.delete('/pedidos/cancelar/:id', authenticateToken, (req, res) => {
     const id = req.params.id;
     const pedidoIndex = pedidos.findIndex(p => p.id === Number(id) && p.usuarioId === req.usuarioAutenticado.id);
     if (pedidoIndex === -1) {
@@ -220,33 +217,29 @@ app.delete('/pedidos/cancelar/:id', (req, res) => {
 });
 
 // Endpoint para deixar uma avaliação para um produto
-app.post('/produtos/:id/avaliacao', (req, res) => {
+app.post('/produtos/:id/avaliacao', authenticateToken, (req, res) => {
     const id = req.params.id;
     const { avaliacao } = req.body;
-    // Você deve implementar a lógica para associar a avaliação ao produto correspondente
-    // Isso pode envolver adicionar a avaliação ao objeto do produto ou armazená-la separadamente
     // Aqui, assumimos que a avaliação é associada ao produto por seu ID
     const produto = produtos.find(p => p.id === id);
     if (!produto) {
         return res.status(404).json({ message: 'Produto não encontrado' });
     }
-    // Armazene a avaliação (você pode adicionar lógica para evitar avaliações duplicadas)
+    // Armazenar
     produto.avaliacao = avaliacao;
     res.status(201).json({ message: 'Avaliação adicionada com sucesso' });
 });
 
 // Endpoint para deixar um comentário em um produto
-app.post('/produtos/:id/comentario', (req, res) => {
+app.post('/produtos/:id/comentario', authenticateToken, (req, res) => {
     const id = req.params.id;
     const { comentario } = req.body;
-    // Você deve implementar a lógica para associar o comentário ao produto correspondente
-    // Isso pode envolver adicionar o comentário ao objeto do produto ou armazená-lo separadamente
     // Aqui, assumimos que o comentário é associado ao produto por seu ID
     const produto = produtos.find(p => p.id === id);
     if (!produto) {
         return res.status(404).json({ message: 'Produto não encontrado' });
     }
-    // Armazene o comentário (você pode adicionar lógica para evitar comentários duplicados)
+    // Armazenar o comentário
     produto.comentario = comentario;
     res.status(201).json({ message: 'Comentário adicionado com sucesso' });
 });
